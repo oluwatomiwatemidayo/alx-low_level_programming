474bb9a0c99f0d30156aa5f8373e7350c2b70b9d
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-03-11 00:36:43 +0100

    C program that prints exactly the word given, follow by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - This is the entry point of the project
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
