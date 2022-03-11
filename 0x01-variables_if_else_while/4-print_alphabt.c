4e80cf495eb2f4221b474a91cf6bbf4e2a2b0119
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-03-11 23:46:20 +0100

    A program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,12 @@ int main(void)
 	int n;
 
 	for (n = 97; n <= 122; ++n)
-		putchar(n);
-	for (n = 65; n <= 90; ++n)
-		putchar(n);
+	{
+		if (n != 101 && n != 113)
+		{
+			putchar(n);
+		}
+	}
 	putchar(10);
 	return (0);
 }
