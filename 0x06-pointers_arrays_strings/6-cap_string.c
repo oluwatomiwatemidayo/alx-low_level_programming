9f3fce8e0a844504a4f9fbe99f6882b8f228590c
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-03-24 15:47:56 +0100

    A function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -29,7 +29,7 @@ char *cap_string(char *str)
 	while (str[++index])
 	{
 		while (!(str[index] >= 'a') && (str[index] <= 'z'))
-			index++;
+			index++; 
 
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
