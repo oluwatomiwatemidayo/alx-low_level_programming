3be9c52b0262346732749033f26f733d8f03d18d
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-03-24 15:54:20 +0100

    A function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * leet - leet encoder
+ * @str: string to be encoded
+ *
+ * Return: address of the encoded string
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = transform(str[i]);
+		i++;
+	}
+	return (str);
+}
+
+/**
+ * transform - helper function to map a letter with it's leet encoding
+ * @x: char to be encoded
+ *
+ * Return: the encoded char
+ */
+
+char transform(char x)
+{
+	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 8)
+	{
+		if (x == mapping_low[i] || x == mapping_upper[i])
+		{
+			replacement = i + '0';
+			break;
+		}
+		i++;
+	}
+	return (replacement);
+}
